a288527c2d3c0a9c805c985e379c9dc717e5258a
Kevin Charm <kevin@kevincharm.com>
2024-02-25 19:25:43 +0100

    add svdw test vectors

--- test/MapToPoint.spec.ts
+++ test/MapToPoint.spec.ts
@@ -2,6 +2,8 @@ import { ethers } from 'hardhat'
 import { MapToPointTest, MapToPointTest__factory } from '../typechain-types'
 import { SignerWithAddress } from '@nomicfoundation/hardhat-ethers/signers'
 import { expect } from 'chai'
+import { hexlify } from 'ethers'
+import SVDW_TEST_VECTORS from './vectors/svdw'
 
 describe('Map-to-point', () => {
     let deployer: SignerWithAddress
@@ -12,30 +14,52 @@ describe('Map-to-point', () => {
     })
 
     it('correctly implements SvdW', async () => {
-        const u = 7105195380181880595384217009108718366423089053558315283835256316808390512725n
-        const [p0Impl, p0Gas] = await mtp.test__svdw_mapToPoint(u)
+        for (const { u, p } of SVDW_TEST_VECTORS) {
+            const [pImpl] = await mtp.test__svdw_mapToPoint(u)
+            expect(pImpl).to.deep.eq(p)
+        }
 
-        console.log(`svdw_mapToPoint(${u}) = ${p0Impl}`)
-        console.log(`p0Gas: ${p0Gas}`)
-        expect(p0Impl[0]).to.eq(
-            15712026073284912390314437469998384224444098668487062629391055065992760594476n,
-        )
-        expect(p0Impl[1]).to.eq(
-            12286200326952730997678485294504458874299852441720220164574895986935631271221n,
-        )
+        // fuzz gas
+        let iterations = 1000n
+        let sumGasCost = 0n
+        for (let i = 0n; i < iterations; i++) {
+            const [, gasCost] = await mtp.test__svdw_mapToPoint(pickRandomF())
+            sumGasCost += gasCost
+        }
+        const meanGasCost = sumGasCost / iterations
+        console.log(`[svdw] mean gas cost: ${meanGasCost}`)
     })
 
     it('correctly implements SSWU', async () => {
         const u = 7105195380181880595384217009108718366423089053558315283835256316808390512725n
-        const [p0Impl, p0Gas] = await mtp.test__sswu_mapToPoint(u)
+        const [p0Impl] = await mtp.test__sswu_mapToPoint(u)
 
-        console.log(`sswu_mapToPoint(${u}) = ${p0Impl}`)
-        console.log(`p0Gas: ${p0Gas}`)
         expect(p0Impl[0]).to.eq(
             7433244435151743403934667274157583038597013229141355912918907345679928483392n,
         )
         expect(p0Impl[1]).to.eq(
             3341345691842296612745507125415299735564087771630588448932624272206506288268n,
         )
+
+        // fuzz gas
+        let iterations = 1000n
+        let sumGasCost = 0n
+        for (let i = 0n; i < iterations; i++) {
+            const [, gasCost] = await mtp.test__sswu_mapToPoint(pickRandomF())
+            sumGasCost += gasCost
+        }
+        const meanGasCost = sumGasCost / iterations
+        console.log(`[sswu] mean gas cost: ${meanGasCost}`)
     })
 })
+
+/// Pick random element from BN254 F_p, accounting for modulo bias
+function pickRandomF(): bigint {
+    for (;;) {
+        const rand32 = crypto.getRandomValues(new Uint8Array(32)) // 256-bit
+        const f = BigInt(hexlify(rand32))
+        if (f < 21888242871839275222246405745257275088696311157297823662689037894645226208583n) {
+            return f
+        }
+    }
+}
