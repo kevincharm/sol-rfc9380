dbdffb01ecdf97674d65cfd76c9418390c2036be
Kevin Charm <kevin@kevincharm.com>
2024-02-25 18:29:12 +0100

    add SvdW

--- contracts/test/MapToPointTest.sol
+++ contracts/test/MapToPointTest.sol
@@ -1,13 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
+import {SVDW} from "../SVDW.sol";
 import {SSWU} from "../SSWU.sol";
 
 contract MapToPointTest {
+    SVDW immutable svdw;
     SSWU immutable sswu;
 
     constructor() {
         sswu = new SSWU();
+        svdw = new SVDW();
+    }
+
+    function test__svdw_mapToPoint(
+        uint256 u
+    ) external view returns (uint256[2] memory p, uint256 gas) {
+        gas = gasleft();
+        p = svdw.mapToPoint(u);
+        gas = gas - gasleft();
     }
 
     function test__sswu_mapToPoint(
--- test/MapToPoint.spec.ts
+++ test/MapToPoint.spec.ts
@@ -11,11 +11,25 @@ describe('Map-to-point', () => {
         mtp = await new MapToPointTest__factory(deployer).deploy()
     })
 
-    it('correctly implements SSWU', async () => {
-        const u = [7105195380181880595384217009108718366423089053558315283835256316808390512725n]
-        const [p0Impl, p0Gas] = await mtp.test__sswu_mapToPoint(u[0])
+    it('correctly implements SvdW', async () => {
+        const u = 7105195380181880595384217009108718366423089053558315283835256316808390512725n
+        const [p0Impl, p0Gas] = await mtp.test__svdw_mapToPoint(u)
 
-        console.log(`mapToPoint(${u[0]}) = ${p0Impl}`)
+        console.log(`svdw_mapToPoint(${u}) = ${p0Impl}`)
+        console.log(`p0Gas: ${p0Gas}`)
+        expect(p0Impl[0]).to.eq(
+            15712026073284912390314437469998384224444098668487062629391055065992760594476n,
+        )
+        expect(p0Impl[1]).to.eq(
+            12286200326952730997678485294504458874299852441720220164574895986935631271221n,
+        )
+    })
+
+    it('correctly implements SSWU', async () => {
+        const u = 7105195380181880595384217009108718366423089053558315283835256316808390512725n
+        const [p0Impl, p0Gas] = await mtp.test__sswu_mapToPoint(u)
+
+        console.log(`sswu_mapToPoint(${u}) = ${p0Impl}`)
         console.log(`p0Gas: ${p0Gas}`)
         expect(p0Impl[0]).to.eq(
             7433244435151743403934667274157583038597013229141355912918907345679928483392n,
